7fabe353bea866316760f006510e13f8af6ee0d8
alejandraarri <aleja.arri@gmail.com>
2018-07-26 19:40:04 +0200

    Sass setup :heart:

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StoreSelector from './components/StoreSelector';
+import "./style.css";
 
 render(<StoreSelector />, document.querySelector('#main'));
