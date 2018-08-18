bf46ca1149fb70dcc0846d82e37f1657d60600a5
alejandraarri <aleja.arri@gmail.com>
2018-08-18 14:36:59 +0200

    Inventory now using styled-components :nail_care:

--- src/components/AddItemForm.js
+++ src/components/AddItemForm.js
@@ -1,10 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 
 class AddItemForm extends React.Component {
   nameRef = React.createRef();
   priceRef = React.createRef();
   statusRef = React.createRef();
-  descRef = React.createRef();
   imageRef = React.createRef();
 
   createItem = (event) => {
@@ -13,7 +13,6 @@ class AddItemForm extends React.Component {
       name: this.nameRef.current.value,
       price: parseFloat(this.priceRef.current.value),
       status: this.statusRef.current.value,
-      desc: this.descRef.current.value,
       image: this.imageRef.current.value
     }
     this.props.addItem(garment);
@@ -22,19 +21,62 @@ class AddItemForm extends React.Component {
 
   render(){
     return (
-        <form className="item__edit-form" onSubmit={this.createItem}>
+        <Form className="item__edit-form" onSubmit={this.createItem}>
           <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
           <input name="price" ref={this.priceRef} type="text" placeholder="Price" />
           <select name="status" ref={this.statusRef}>
             <option value="available">In Stock</option>
             <option value="unavailable">Sold Out</option>
           </select>
-          <textarea name="desc" ref={this.descRef} placeholder="Desc" ></textarea>
           <input name="image" ref={this.imageRef} type="text" placeholder="Image" />
           <button type="submit">Add Garment</button>
-        </form>
+        </Form>
     );
   }
 }
+
+const Form = styled.form`
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  width: 80vw;
+  max-width: 500px;
+  margin: 0 auto;
+  margin-bottom: 20px;
+  border: 1px solid rgba(0, 40, 100, 0.12);
+  border-radius: 3px;
+  overflow: hidden;
+
+  input, select {
+    width: 33.33%;
+    background: #fff;
+    border: 0;
+    border-bottom: 1px solid rgba(0, 40, 100, 0.12);
+    border-radius: 0;
+    padding: 10px;
+    font-size: 1em;
+    line-height: 1;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+  }
+  input:nth-child(2) {
+    border-right: 1px solid rgba(0, 40, 100, 0.12);
+    border-left: 1px solid rgba(0, 40, 100, 0.12);
+  }
+  input:last-of-type {
+    width: 100%;
+  }
+  button {
+    width: 100%;
+    border: 0;
+    padding: .5em;
+
+    &:hover {
+      background-color: #f6f6f6;
+    }
+  }
+`;
 
 export default AddItemForm;
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,19 +1,62 @@
 import React, { Fragment } from "react";
 import AddItemForm from './AddItemForm';
+import styled from "styled-components";
 
 class Inventory extends React.Component {
   render(){
     return (
       <Fragment>
-        <input type="checkbox" id="inventory-toggle"/>
-        <div className="inventory">
-          <label className="inventory__toggle" htmlFor="inventory-toggle"></label>
-          <h2 className="inventory__heading">Inventory</h2>
+        <InventoryCheckbox type="checkbox" id="inventory-toggle" />
+        <InventoryWrapper>
+          <InventoryToogle htmlFor="inventory-toggle"></InventoryToogle>
+          <InventoryHeading>Inventory</InventoryHeading>
           <AddItemForm addItem={this.props.addItem}/>
-        </div>
+        </InventoryWrapper>
       </Fragment>
     );
   }
 }
 
+const InventoryCheckbox = styled.input`
+  display: none;
+`;
+
+const InventoryWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  max-height: 70px;
+  background-color: #f8f9fa;
+  border-top: 1px solid rgba(0, 40, 100, 0.12);
+  text-align: center;
+  transition: max-height 0.5s ease-out;
+
+  ${InventoryCheckbox}:checked + & {
+    max-height: 500px;
+    transition: max-height 0.5s ease-out;
+  }
+`;
+
+const InventoryToogle = styled.label`
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 1em;
+  font-size: 1.5em;
+  color: #777;
+  cursor: pointer;
+  transform:rotate(-90deg);
+
+  &::before {
+    content: '\00bb';
+  }
+
+  ${InventoryCheckbox}:checked + ${InventoryWrapper} & {
+    transform:rotate(90deg);
+  }
+`;
+
+const InventoryHeading = styled.h2`
+  font-weight: 500;
+`;
 export default Inventory;
